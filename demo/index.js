fc3f93f2ab7b2416ed1fe69327bf28f22885a582
huainanzi <huainanzi@wacai.com>
2019-03-07 10:35:50 +0800

    5.runtime打包

--- demo/index.js
+++ demo/index.js
@@ -22,8 +22,10 @@ console.log(assistEs6.key)
 * 当没babel是空对象{}的时候 let const 都没有转化
 * 当加入preset的时候 let -> var; => -> function;
 * 当加入polyfill的时候 代码扩大的好多
+* 当使用runtime的时候 我babel7没有试出来 用了babel 6套餐 以及 webpack3 做出来了
 */
-import '@babel/polyfill'
+
+// import '@babel/polyfill'
 
 let func = () => { console.log('babel function arrow')};
 const NUM = 45;
@@ -37,3 +39,23 @@ let p = new Promise((resolve) => {
 }).then(data => {
     console.log('resolve data', data)
 })
+
+/*****   react   ******/
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import { Provider } from 'react-redux'
+// import AppContext from './common/context'
+
+// import store from './store'
+// import AppRouter from './common/routes'
+// import history from './common/history'
+
+// ReactDOM.render(
+//     <AppContext.Provider value={{ testContext: true }}>
+//         <Provider store={store}>
+//             <AppRouter history={history} />
+//         </Provider>
+//     </AppContext.Provider>,
+//     document.getElementById('app')
+// )
