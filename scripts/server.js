e96e96c6b735344b138fca49a6f7906e2da3d568
huainanzi <huainanzi@wacai.com>
2019-03-13 17:46:10 +0800

    本地玩玩转demo

--- scripts/server.js
+++ scripts/server.js
@@ -1,18 +1,28 @@
+const express = require('express')
 const path = require('path')
-const webpack = require('webpack')
-const WebpackDevServer = require('webpack-dev-server')
+const port = process.env.PORT || 8081
+const app = express()
+var fs=require("fs");
+// 在你应用 JavaScript 文件中包含了一个 script 标签
+// 的 index.html 中处理任何一个 route
+app.get('*', function (request, response){
+  if (request.path.endsWith('.js') || request.path.endsWith('.css') || request.path.endsWith('.less') || request.path.endsWith('.png') || request.path.endsWith('.jpeg') || request.path.endsWith('.jpg')) {
 
-const configPath = path.resolve(__dirname, '../', 'webpack.config.js')
-const config = require(configPath)
-
-const compiler = webpack(config)
-
-const server = new WebpackDevServer(compiler)
-
-compiler.hooks.done.tap('compilation', stats => {
-  console.log(`webpack build success`)
+    fs.exists(path.resolve(__dirname, '../dist', request.path.substr(1, request.path.length - 1)), function(exists){
+      if(exists){
+         console.log("文件存在")
+      }
+      if(!exists){
+         console.log("文件不存在")
+      }
+    })
+  
+    response.sendFile(path.resolve(__dirname, '../dist', request.path.substr(1, request.path.length - 1)))
+    return
+  }
+  // console.log(request)
+  response.sendFile(path.resolve(__dirname, '../dist', 'index.html'))
 })
 
-server.listen(8080, null, () => {
-  console.log(`webpack-dev-server started at localhost: 8080`)
-})
+app.listen(port)
+console.log("server started on port " + port)
